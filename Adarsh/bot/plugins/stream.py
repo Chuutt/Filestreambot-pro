f019a734690babddcca9687a2753f7c7ec852371
Chuutt <93823376+Chuutt@users.noreply.github.com>
2022-08-21 18:15:16 +0530

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -158,8 +158,8 @@ async def channel_receive_handler(bot, broadcast):
             message_id=broadcast.id,
             reply_markup=InlineKeyboardMarkup(
                 [
-                    [InlineKeyboardButton("Watch Online 👨🏻‍💻", url=stream_link)],
-                     [InlineKeyboardButton('Direct Downld 📥', url=online_link)] 
+                    [InlineKeyboardButton("🖥STREAM ", url=stream_link),
+                     InlineKeyboardButton('Dᴏᴡɴʟᴏᴀᴅ📥', url=online_link)] 
                 ]
             )
         )
